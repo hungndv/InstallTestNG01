54da6a5e83c5015ad2deb155ea8d8a2b0e49c379
Hung Duc Vinh Nguyen <hung.duc.vinh.nguyen@steadfasttech.com.au>
2024-06-08 15:28:23 +0700

    first commit

---
+++ src/com/examples/hungndv/TestClass01.java
@@ -0,0 +1,11 @@
+package com.examples.hungndv;
+
+import org.testng.annotations.Test;
+
+public class TestClass01 {
+	
+	@Test
+	public void saySomething() {
+		System.out.println("Hello world!");
+	}
+}
